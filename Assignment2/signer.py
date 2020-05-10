1b622c9a95908f156124b39436d2d9b952f2eaa8
ShivamMalhotra1994 <shivammalhotra08@gmail.com>
2020-05-11 00:20:01 +0530

    Added Auto Append to JSON

--- Assignment2/signer.py
+++ Assignment2/signer.py
@@ -2,6 +2,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from Crypto.Hash import SHA256
 import binascii
+import json
 
 f = open('myPrivateKey.pem','r')
 key = RSA.import_key(f.read())
@@ -11,4 +12,11 @@ hash = SHA256.new(msg)
 signer = PKCS115_SigScheme(key)
 signature = signer.sign(hash)
 print("Signature:", binascii.hexlify(signature).decode('ASCII'))
+newpair = {msg.decode('ASCII'): binascii.hexlify(signature).decode('ASCII')}
+
+with open("data.json", "r+") as file:
+    data = json.load(file)
+    data.update(newpair)
+    file.seek(0)
+    json.dump(data, file)
 
